b0c47272801fe2b43df42697e651c4fe1bf41b3d
David Nadlinger <code@klickverbot.at>
2014-06-18 23:04:07 +0200

    Simple application for running tests locally.

--- run-single/app.d
+++ run-single/app.d
@@ -1,2 +1,33 @@
-void main() {
+int main(string[] args) {
+    import dash.benchmark.spec;
+    import dash.compiler;
+    import std.array : array;
+    import std.algorithm : map;
+    import std.stdio;
+
+    if (args.length < 4) {
+        writeln("Usage: ", args[0], " <benchmarkDir> <compilerType> <compilerExe>");
+        return 1;
+    }
+
+    immutable benchmarkDir = args[1];
+
+    import dash.compiler.dmd;
+    import dash.compiler.ldc;
+    Compiler function(string)[string] compilerConstructors = [
+        "dmd": a => new DMD(a),
+        "ldc": a => new LDC(a)
+    ];
+
+    auto c = args[2] in compilerConstructors;
+    if (!c) {
+        writeln("Unknown compiler type, must be one of ",
+            compilerConstructors.keys);
+    }
+    auto compiler = (*c)(args[3]);
+
+    auto spec = readSpec(benchmarkDir);
+    auto results = spec.tests.map!(a => a.execute(compiler, null)).array;
+
+    return 0;
 }
